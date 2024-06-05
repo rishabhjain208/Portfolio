150e54a657305c83a2ddd9582ed77c29e0b628b8
Rishabh Jain <107530140+rishabhjain208@users.noreply.github.com>
2024-06-05 21:29:24 +0530

    Update Techstack.js

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -6,78 +6,67 @@ import {
   DiReact,
   DiNodejs,
   DiMongodb,
-  DiPython,
-  DiGit,
   DiJava,
 } from "react-icons/di";
 import {
-  SiRedis,
-  SiFirebase,
   SiNextdotjs,
-  SiSolidity,
+  SiTypescript,
+  SiExpress,
+  SiTailwindcss,
   SiPostgresql,
+  SiPrisma,
 } from "react-icons/si";
-import { TbBrandGolang } from "react-icons/tb";
-import { SiTypescript } from "react-icons/si";
-import { SiExpress } from "react-icons/si";
-import { SiTailwindcss } from "react-icons/si";
-import { SiPrisma } from "react-icons/si";
+
 function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
         <CgCPlusPlus />
-        <span className="text-white">C++</span>
+        <span style={{ fontSize: "12px", color: "white" }}>C++</span>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiJava />
-        <span className="text-white">Java</span>
+        <span style={{ fontSize: "12px", color: "white" }}>Java</span>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiJavascript1 />
-        <span className="text-white">JavaScript</span>
+        <span style={{ fontSize: "12px", color: "white" }}>JavaScript</span>
       </Col>
-      {/* <Col xs={4} md={2} className="tech-icons">
-        <TbBrandGolang />
-      </Col> */}
       <Col xs={4} md={2} className="tech-icons">
         <DiNodejs />
-        <span className="text-white">Nodejs</span>
+        <span style={{ fontSize: "12px", color: "white" }}>Nodejs</span>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiReact />
-        <span className="text-white">ReactJS</span>
+        <span style={{ fontSize: "12px", color: "white" }}>ReactJS</span>
       </Col>
-      {/* <Col xs={4} md={2} className="tech-icons">
-        <SiSolidity />
-      </Col> */}
       <Col xs={4} md={2} className="tech-icons">
         <DiMongodb />
-        <span className="text-white">MongoDB</span>
+        <span style={{ fontSize: "12px", color: "white" }}>MongoDB</span>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiNextdotjs />
-        <span className="text-white">Nextjs</span>
+        <span style={{ fontSize: "12px", color: "white" }}>Nextjs</span>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiTypescript />
-        <span className="text-white">TypeScript</span>
+        <span style={{ fontSize: "12px", color: "white" }}>TypeScript</span>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiExpress />
-        <span className="text-white">ExpressJS</span>
+        <span style={{ fontSize: "12px", color: "white" }}>ExpressJS</span>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiTailwindcss />
-        <span className="text-white">TailwindCSS</span>
+        <span style={{ fontSize: "12px", color: "white" }}>TailwindCSS</span>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiPostgresql />
-        <span className="text-white">Postgresql</span>
+        <span style={{ fontSize: "12px", color: "white" }}>Postgresql</span>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiPrisma />
-        <span className="text-white">Prisma</span>
+        <span style={{ fontSize: "12px", color: "white" }}>Prisma</span>
       </Col>
     </Row>
   );
